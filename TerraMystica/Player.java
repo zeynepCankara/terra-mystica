00bbd6437a63808cc78ea66d2d72355331957cbd
hebzatron <59509536+hebzatron@users.noreply.github.com>
2020-04-02 22:18:39 +0300

    Add files via upload

---
+++ TerraMystica/Player.java
@@ -0,0 +1,63 @@
+import java.util.*;
+package TerraMystica;
+public class Player {
+	//Variables
+	private String faction;
+	private HashMap<Structure,Integer> availableStructures;
+	private HashMap<Structure,Integer> structesBuilt;
+	private ArrayList<Terrain> ownedTerrains;
+	private int score;
+	private int coins;
+	private int numOfPriests;
+	private int numOfWorkers;
+	private HashMap<Integer,Integer> powerBowlDistribution;
+	private ArrayList<Terrain> adjacencyList;
+	private int shipping;
+	private int spadeRate;
+	private int numOfTownKeys;
+	public boolean transformTerrain(int Terrain)
+	{
+		return true;
+	}
+	public boolean build(int Terrain)
+	{
+		return true;
+	}
+	public boolean improveShipping()
+	{
+		return true;
+	}
+	public boolean improveTerraforming()
+	{
+		return true;
+	}
+	public boolean upgradeStructure(int Structure)
+	{
+		return true;
+	}
+	public boolean sendPriestToCult(int cultID)
+	{
+		return true;
+	}
+	public boolean usePowerAction(int powerActionID)
+	{
+		return true;
+	}
+	public boolean powerBowlConvert()
+	{
+		return true;
+	}
+	public boolean useSpecialAction(int specialActionID)
+	{
+		return true;
+	}
+	public void pass()
+	{
+		
+	}
+	public void getResources()
+	{
+		
+	}
+
+}	
