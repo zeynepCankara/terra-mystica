ba0db2fdfc3c63b6f6c50535b3fc703c4f0c9cd2
raficoktalas <rafi@coktalas.com>
2020-05-16 18:49:18 +0300

    4th level of adjacency is added.

--- src/main/java/gameLogicManager/gameControllerManager/AdjacencyController.java
+++ src/main/java/gameLogicManager/gameControllerManager/AdjacencyController.java
@@ -37,8 +37,16 @@ public class AdjacencyController{
         int[] level1 = {-25,-14,-11,11,14,25};
         int[] level2 = {-26,-24,-2,2,24,26};
         int[] level3 = {-38,-37,-27,-23,-15,-10,10,15,23,27,37,38};
-        int[] level4 = {};//TODO
+        int[] level4 = {-39,-36,-3,3,36,39};
         switch(player.getShipping()) {
+            case 4:
+                for( int i = 0; i < 6; i++ ){
+                    if(terrain.getId()+level4[i]<113 && terrain.getId()+level4[i]>=0 ){
+                        if( terrainList[terrain.getId()+level4[i]].getOwner()==player ){
+                            return true;
+                        }
+                    }
+                }
             case 3:
                 for( int i = 0; i < 12; i++ ){
                     if(terrain.getId()+level3[i]<113 && terrain.getId()+level3[i]>=0 ){
